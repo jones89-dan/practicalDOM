28e2440eb846fe826016d81caf5e3f292feec361
Daniel <jones.89.dan@gmail.com>
2021-10-10 15:52:09 -0700

    Calculate unrealized gain/loss based on market value, cost per share, and shares owned

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-$(document).ready(function () {
+  var updateMarketValue = function (ele) {
+    var sharesOwned = parseFloat($(ele).children('.shares').text());
+    var marketPrice = parseFloat($(ele).children('.marketPrice').text());
+  
+    // market value is shares times market price per share
+    var marketValue = sharesOwned * marketPrice;
+    $(ele).children('.marketValue').html(marketValue);
+  
+    return marketValue;
+  }
+  
+  var updateUnrealizedProfit = function (ele, marketValue) {
+    var sharesOwned = parseFloat($(ele).children('.shares').text());
+    var costPerShare = parseFloat($(ele).children('.cost').text());
+    var costOfPurchase = sharesOwned * costPerShare;
+  
+    // unrealized profit is market value minus cost of purchase
+    var unrealizedProfit = marketValue - costOfPurchase;
+    $(ele).children('.profit').html(unrealizedProfit);
+  
+    return unrealizedProfit;
+  }
+  
+  $(document).ready(function () {
     $('tbody tr').each(function (i, ele) {
-
-        var sharesOwned = parseFloat($(ele).children('.shares').text());
-        var marketPrice = parseFloat($(ele).children('.marketPrice').text());
-        // market value is number of shares times market price per share
-       
-        var marketValue = sharesOwned * marketPrice;
-        $(ele).children('.marketValue').html(marketValue);
+      var marketValue = updateMarketValue(ele);
+      updateUnrealizedProfit(ele, marketValue);
     });
   });
   
