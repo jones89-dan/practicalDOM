1f6d86b81830d1ca697a1d872f870433ccdbcd35
Daniel <jones.89.dan@gmail.com>
2021-10-10 15:49:08 -0700

    Add market value HTML to the DOM

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
     $('tbody tr').each(function (i, ele) {
-        
+
         var sharesOwned = parseFloat($(ele).children('.shares').text());
         var marketPrice = parseFloat($(ele).children('.marketPrice').text());
         // market value is number of shares times market price per share
+       
         var marketValue = sharesOwned * marketPrice;
-        console.log(marketValue);
+        $(ele).children('.marketValue').html(marketValue);
     });
   });
   
